a545151ac647f7395a60be0f6eec59d932fad464
C0dex73 <c0d3x73@gmail.com>
2025-01-22 00:27:44 +0100

    changed adress in html and added adress copy mechanic

--- js/index.js
+++ js/index.js
@@ -41,3 +41,29 @@ var getServerStatus = ()=>{
 jQuery(()=>{
     getServerStatus();
 })
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+var clickedRecently = false
+var adressCopyButtonClick = async (e) => {
+    //anti-spam
+    if(clickedRecently) { return; }
+    clickedRecently = true
+
+    // Use the Clipboard API
+    navigator.clipboard.writeText($('#server-adress serverAdress').text())
+
+    //animate 'Copied !' text
+    $('#server-adress>p').css('display', 'block');
+    for (let index = 0; index < 300; index++) {
+        $('#server-adress>p').css('top', (e.clientY - index/7).toString()+"px");
+        $('#server-adress>p').css('left', (e.clientX).toString()+"px");
+        await delay(1)
+    }
+    $('#server-adress>p').css('display', 'none');
+    clickedRecently = false
+}
+
+$(document).ready(function () {
+    $('#server-adress').on("click", adressCopyButtonClick);
+});
